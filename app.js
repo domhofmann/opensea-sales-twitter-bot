876748180d3cb897d46dfef3a901696c8826c345
Dom Hofmann <sevenflow@gmail.com>
2021-08-04 15:52:49 -0400

    Fix typo

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ function formatAndSendTweet(event) {
     const totalPrice = _.get(event, 'total_price');
     const usdValue = _.get(event, ['payment_token', 'usd_price']);
     const tokenSymbol = _.get(event, ['payment_token', 'symbol']);
-    const buyer = _.get(event, ['winner_account', 'user', 'username'], 'Anonymous');
-    const seller = _.get(event, ['seller', 'user', 'username'], 'Anonymous');
+    const buyer = _.get(event, ['winner_account', 'user', 'username'], 'Anonymous') || 'Anonymous';
+    const seller = _.get(event, ['seller', 'user', 'username'], 'Anonymous') || 'Anonymous';
 
     const formattedTokenPrice = ethers.utils.formatEther(totalPrice.toString());
     const formattedUsdPrice = (formattedTokenPrice * usdValue).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
